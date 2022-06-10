f611d160a4e8ffecb45f18df32f18535de468e9f
Jodu555 <Jodu505@gmail.com>
2022-06-10 12:04:15 +0200

    tested a bit the vue3 swal

--- client/src/main.js
+++ client/src/main.js
@@ -3,4 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+const app = createApp(App);
+app.use(VueSweetalert2);
+app.use(store);
+app.use(router);
+app.mount('#app');
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,26 +1,29 @@
 <template>
-	<div class="container accordion accordion-flush" id="accordionFlushExample">
-		<div v-for="categorie of categories" :key="categorie.title" class="accordion-item">
-			<h2 class="accordion-header">
-				<button
-					class="accordion-button collapsed"
-					type="button"
-					data-bs-toggle="collapse"
-					:data-bs-target="'#flush-' + categorie.title"
-					aria-expanded="false"
-					:aria-controls="'flush-' + categorie.title"
-				>
-					{{ categorie.title }} / {{ categorie.entitys.length }}
-				</button>
-			</h2>
-			<div :id="'flush-' + categorie.title" class="accordion-collapse collapse">
-				<div class="accordion-body">
-					<div class="row row-cols-1 row-cols-md-3 g-4">
-						<EntityCard
-							v-for="entity in categorie.entitys"
-							:entity="entity"
-							:key="entity.title"
-						></EntityCard>
+	<div>
+		<button class="btn btn-outline-success" @click="toastTopEnd">Pop</button>
+		<div class="container accordion accordion-flush" id="accordionFlushExample">
+			<div v-for="categorie of categories" :key="categorie.title" class="accordion-item">
+				<h2 class="accordion-header">
+					<button
+						class="accordion-button collapsed"
+						type="button"
+						data-bs-toggle="collapse"
+						:data-bs-target="'#flush-' + categorie.title"
+						aria-expanded="false"
+						:aria-controls="'flush-' + categorie.title"
+					>
+						{{ categorie.title }} / {{ categorie.entitys.length }}
+					</button>
+				</h2>
+				<div :id="'flush-' + categorie.title" class="accordion-collapse collapse">
+					<div class="accordion-body">
+						<div class="row row-cols-1 row-cols-md-3 g-4">
+							<EntityCard
+								v-for="entity in categorie.entitys"
+								:entity="entity"
+								:key="entity.title"
+							></EntityCard>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -49,7 +52,19 @@ export default {
 			return categories;
 		},
 	},
-	async mounted() {},
+	methods: {
+		toastTopEnd() {
+			this.$swal({
+				toast: true,
+				position: 'top-end',
+				showConfirmButton: false,
+				timer: 1000,
+				icon: 'success',
+				title: 'Saved',
+				timerProgressBar: true,
+			});
+		},
+	},
 };
 </script>
 <style lang=""></style>
