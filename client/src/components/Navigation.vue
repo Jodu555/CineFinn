adfea4c4c089c0d6917ae9aedefe07d1ef91a2e3
Jodu555 <Jodu505@gmail.com>
2022-06-11 03:43:01 +0200

    get the bootstrap5-autocomplete working

--- client/src/components/Navigation.vue
+++ client/src/components/Navigation.vue
@@ -20,6 +20,13 @@
 					</li>
 				</ul>
 				<div class="d-flex">
+					<input
+						type="text"
+						class="form-control"
+						id="input"
+						placeholder="Start typing..."
+						autocomplete="off"
+					/>
 					<div class="btn-group" role="group" aria-label="Basic outlined example">
 						<button class="btn btn-outline-primary">Profile</button>
 						<button class="btn btn-outline-danger" type="submit">Logout</button>
@@ -30,7 +37,36 @@
 	</nav>
 </template>
 
-<script></script>
+<script>
+import Autocomplete from '@/plugins/autocomplete';
+export default {
+	mounted() {
+		const field = document.getElementById('input');
+		const ac = new Autocomplete(field, {
+			data: [{ label: "I'm a label", value: 42 }],
+			maximumItems: 5,
+			threshold: 1,
+			onSelectItem: ({ label, value }) => {
+				console.log('user selected:', label, value);
+			},
+		});
+		ac.setData([
+			{
+				label: 'Test',
+				value: 'test',
+			},
+			{
+				label: 'hello',
+				value: 'hello',
+			},
+			{
+				label: 'other',
+				value: 'other',
+			},
+		]);
+	},
+};
+</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss"></style>
