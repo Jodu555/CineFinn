9f28d1ac4f87c5ce6bef64796310b029d9edc4a6
Jodu555 <Jodu505@gmail.com>
2022-06-26 06:05:40 +0200

    added the naive solution

--- client/src/main.js
+++ client/src/main.js
@@ -8,13 +8,19 @@ import 'sweetalert2/dist/sweetalert2.min.css';
 
 import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
 
+const networkingPlugin = {
+    install: (app, options) => {
+        app.config.globalProperties.$networking = new Networking('http://localhost:3100', '');
+    }
+}
+
 const app = createApp(App);
 app.use(VueSweetalert2);
 app.use(autoAnimatePlugin);
+app.use(networkingPlugin)
 app.use(store);
 app.use(router);
 
 store.$app = app;
 store.$networking = app.config.globalProperties.$networking;
-
 app.mount('#app');
