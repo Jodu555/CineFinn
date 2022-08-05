6b3954520abc33434f38e2a432cb97cea6b85d41
Jodu555 <Jodu505@gmail.com>
2022-08-05 17:14:22 +0200

    integrated the socket.io-client

--- client/src/main.js
+++ client/src/main.js
@@ -8,14 +8,41 @@ import 'sweetalert2/dist/sweetalert2.min.css';
 
 import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
 
+import { io } from "socket.io-client";
+
+import "bootstrap"
+
+const serverURL = location.hostname == 'localhost' ? 'http://localhost:3200' : 'http://cinema-api.jodu555.de';
+
+
+const socketPlugin = {
+    install: (app, options) => {
+        app.config.globalProperties.$socket = io(serverURL);
+    }
+}
+
 const networkingPlugin = {
     install: (app, options) => {
-        const aBase = location.hostname == 'localhost' ? 'http://localhost:3100' : 'http://cinema-api.jodu555.de'
-        app.config.globalProperties.$networking = new Networking(aBase, '');
+        app.config.globalProperties.$networking = new Networking(serverURL, '');
     }
 }
 
 const app = createApp(App);
+
+app.use(socketPlugin);
+
+const socket = app.config.globalProperties.$socket;
+
+socket.on("connect_error", async (err) => {
+    console.log('Socket Connect Error: ', err.message); // prints the message associated with the error
+    if (err.message.includes('Authentication'))
+        await store.dispatch('auth/authenticate');
+});
+
+socket.on('connect', () => {
+    console.log('Socket Connect Success');
+})
+
 app.use(VueSweetalert2);
 app.use(autoAnimatePlugin);
 app.use(networkingPlugin)
@@ -23,4 +50,5 @@ app.use(store);
 app.use(router);
 
 store.$networking = app.config.globalProperties.$networking;
+store.$socket = app.config.globalProperties.$socket;
 app.mount('#app');
