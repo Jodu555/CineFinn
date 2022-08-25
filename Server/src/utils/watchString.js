2b8d05af0ecd492897ebf81b111e71b186b22462
Jodu555 <Jodu505@gmail.com>
2022-08-25 10:37:03 +0200

    changed the idea on how to deal with the watch str

--- Server/src/utils/watchString.js
+++ Server/src/utils/watchString.js
@@ -1,9 +1,26 @@
 const generate = () => {
     //TODO: Generate the watch string by input
+    console.log('FN call');
 }
 
 const parse = (str) => {
     //TODO: Parse the watch string to the infos
+    const list = str.split(';');
+    // 'ID:se-ep.time;next'
+    // res.json([
+    //     '781|1.0',
+    //     '781|1.1',
+    //     '781|1.2',
+    //     '781|1.3',
+    //     '781|1.5',
+    //     '781|1.6',
+    //     '781|1.9',
+    // ])
+    console.log(list);
 }
 
-generate();
+const load = (UUID) => {
+    //TODO: Returns the current watch string
+}
+
+parse('781:1-1.0;781:1-2.50');
