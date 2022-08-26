f750712d0e9c6fb12677becdbe11b418afcfb8d9
Jodu555 <Jodu505@gmail.com>
2022-08-26 08:40:02 +0200

    wrote the jsdoc for these functions and improved the behaviour

--- Server/src/utils/watchString.js
+++ Server/src/utils/watchString.js
@@ -1,16 +1,32 @@
-const generate = (segmentList) => {
+/**
+ * @param  {[Segment]} segmentList
+ * @returns {String}
+ */
+const generateStr = (segmentList) => {
     //TODO: Generate the watch string by the segmentList input
+    let str = '';
+    segmentList.forEach(segment => {
+        console.log(segment);
+        str += `${segment.ID}:${segment.season}-${segment.episode}.${segment.time};`;
+    });
+    console.log(str);
+    return str;
 }
 
 // 'ID:se-ep.time;repeat'
+/**
+ * @param  {String} str
+ * @returns {[Segment]}
+ */
 const parse = (str) => {
     //TODO: Parse the watch string to the infos
     const list = str.split(';').map(s => {
+        if (!(s.length > 0)) return null;
         const collenSplit = s.split(':');
         const dashSplit = collenSplit[1].split('-')
         const dotSplit = dashSplit[1].split('.')
         return new Segment(collenSplit[0], dashSplit[0], dotSplit[0], dotSplit[1]);
-    });
+    }).filter(s => s != null);
     return list;
 }
 
@@ -29,4 +45,4 @@ const load = (UUID) => {
 
 const segList = parse('781:1-1.0;781:1-2.50');
 
-generate(segList);
+console.log(parse(generateStr(segList)));
