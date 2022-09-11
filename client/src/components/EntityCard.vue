d107a91653e1cf77ce04b93ba29ad6d5ec38f5ad
Jodu555 <Jodu505@gmail.com>
2022-09-11 23:53:21 +0200

    added some fallback stuff

--- client/src/components/EntityCard.vue
+++ client/src/components/EntityCard.vue
@@ -11,16 +11,19 @@
 				class="card-img-top"
 				alt="..."
 			/> -->
-			<img v-if="entity.infos.image" :src="entity.infos.image" class="card-img-top" alt="..." />
+			<img v-if="entity?.infos?.image" :src="entity.infos.image" class="card-img-top" alt="..." />
 			<div class="card-body">
 				<h4 class="card-title">{{ entity.title }}</h4>
-				<p class="card-text">
+				<p v-if="entity.infos != {}" class="card-text">
 					{{ entity.infos.description }}
 					<br />
+					<!-- <pre>{{ entity.infos}}</pre> -->
 					<small class="text-muted"
 						>{{ entity.infos.startDate }} - {{ entity.infos.endDate }}</small
 					>
-					<!-- <pre>{{entity.infos}}</pre> -->
+				</p>
+				<p v-else class="card-text">
+					With supporting text below as a natural lead-in to additional content.
 				</p>
 				<router-link class="btn btn-outline-primary btn-sm" :to="'/watch?id=' + entity.ID"
 					>Go & Watch</router-link
