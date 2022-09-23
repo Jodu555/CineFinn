2749d7e236539cbd01627dfb2e0a6d3ad6bb7cfa
Jodu555 <Jodu505@gmail.com>
2022-09-23 11:07:31 +0200

    added a highly more intuitive regexparser for the filename parsing

--- Server/src/classes/series.js
+++ Server/src/classes/series.js
@@ -26,12 +26,27 @@ const cleanupSeriesBeforeFrontResponse = (series) => {
 
 const filenameParser = (filepath, filename) => {
     // filename exp. Food Wars! Shokugeki no Sōma St#1 Flg#1.mp4
-    if (filename.includes('St#') && filename.includes('Flg#')) {
-        const [title, rest] = filename.split('St#')
-        const [season, rest2] = rest.split(' ');
-        const episode = rest2.split('#')[1].split('.')[0];
-        // console.log(episode);
+    // if (filename.includes('St#') && filename.includes('Flg#')) {
+    //     const [title, rest] = filename.split('St#')
+    //     const [season, rest2] = rest.split(' ');
+    //     const episode = rest2.split('#')[1].split('.')[0];
+    //     // console.log(episode);
 
+    //     return { movie: false, title: title.trim(), season: Number(season), episode: Number(episode) };
+    // } else {
+    //     const title = path.basename(path.dirname(path.dirname(filepath)));
+    //     return { movie: true, title, movieTitle: filename }
+    // }
+
+    const re = /^(.*)St#(\d+) Flg#(\d+).mp4/ig;
+    const match = re.exec(filename);
+    if (match != null) {
+        const [
+            original,
+            title,
+            season,
+            episode,
+        ] = match
         return { movie: false, title: title.trim(), season: Number(season), episode: Number(episode) };
     } else {
         const title = path.basename(path.dirname(path.dirname(filepath)));
