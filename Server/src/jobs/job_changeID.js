3cc120729afea2b427b9924bb5ce1f3bfdcc853d
Jodu555 <Jodu505@gmail.com>
2022-10-17 13:57:06 +0200

    worked on the changeID job

--- Server/src/jobs/job_changeID.js
+++ Server/src/jobs/job_changeID.js
@@ -1,5 +1,15 @@
 const fs = require('fs');
+const { load, parse } = require('../utils/watchString');
 
+
+const getChangesMap = (from, to) => {
+    const changes = new Map();
+    from.forEach(se => {
+        const cse = to.find(s => se.title == s.title);
+        changes.set(se.ID, cse.ID);
+    });
+    return changes
+}
 
 module.exports = {
     //This job i wrote cause aproximately it happens over and over that i tweak the series object definition
@@ -8,11 +18,25 @@ module.exports = {
         options: ['from', 'to']
     },
     run: (options) => {
+        const { Database } = require('@jodu555/mysqlapi');
+        const database = Database.createDatabase(process.env.DB_HOST, process.env.DB_USERNAME, process.env.DB_PASSWORD, process.env.DB_DATABASE);
+        database.connect();
+        require('../utils/database')();
 
-        console.log(process.env.PREVIEW_IMGS_PATH);
+        const from = JSON.parse(fs.readFileSync(options.from));
+        const to = JSON.parse(fs.readFileSync(options.to));
 
-        //Change in the config
+        const changes = getChangesMap(from, to);
+
+        console.log(changes);
+
+        const watchString = load(/** user_account */);
+        const watchSegmentList = parse(watchString);
+
+
+
         //Change the preview images folder
+        //Change the watchlist
 
     }
 }
