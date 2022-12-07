e1a4196e66eec36d7e7a705b7ebe68ff4512032c
Jodu555 <Jodu505@gmail.com>
2022-12-07 02:01:26 +0100

    Linted

--- Server/src/routes/watch.js
+++ Server/src/routes/watch.js
@@ -2,12 +2,11 @@ const express = require('express');
 const { load, parse } = require('../utils/watchString');
 const router = express.Router();
 
-
 router.get('/info', async (req, res, next) => {
-    // 'ID|se-ep;next'
-    console.log('/watch/info', req.credentials.user.username, req.query.series);
-    const segList = parse(await load(req.credentials.user.UUID));
-    res.json(segList.filter(seg => seg.ID == req.query.series));
+	// 'ID|se-ep;next'
+	console.log('/watch/info', req.credentials.user.username, req.query.series);
+	const segList = parse(await load(req.credentials.user.UUID));
+	res.json(segList.filter((seg) => seg.ID == req.query.series));
 });
 
-module.exports = { router }
+module.exports = { router };
