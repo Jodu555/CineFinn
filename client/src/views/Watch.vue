63c13a1d76a8187323d18fee25fb6ec23817ed6e
Jodu555 <Jodu505@mail.com>
2022-12-24 17:49:57 +0100

    Update WIP

--- client/src/views/Watch.vue
+++ client/src/views/Watch.vue
@@ -25,7 +25,7 @@
 			<div v-auto-animate v-if="showLatestWatchButton" class="text-center">
 				<button @click="skipToLatestTime" class="btn btn-outline-info">Jump to Latest watch position!</button>
 			</div>
-			<div class="d-flex justify-content-end">
+			<!-- <div class="d-flex justify-content-end">
 				<div class="btn-group">
 					<button
 						class="btn btn-outline-primary dropdown-toggle"
@@ -40,15 +40,9 @@
 					<div class="dropdown-menu" aria-labelledby="seenmark">
 						<button type="button" class="dropdown-item" :class="{ disabled: currentSeason == -1 }"><b>Gesehen</b></button>
 						<button type="button" class="dropdown-item" :class="{ disabled: currentSeason == -1 }"><b>Nicht gesehen</b></button>
-
-						<!-- <button type="button" class="dropdown-item" :class="{ disabled: currentSeason == -1 }">Staffel als <b>gesehen</b> markieren</button>
-						<button type="button" class="dropdown-item" :class="{ disabled: currentEpisode == -1 }">Folge als <b>gesehen</b> markieren</button>
-						<div class="dropdown-divider"></div>
-						<button type="button" class="dropdown-item" :class="{ disabled: currentSeason == -1 }">Staffel als <b>nicht gesehen</b> markieren</button>
-						<button type="button" class="dropdown-item" :class="{ disabled: currentEpisode == -1 }">Folge als <b>nicht gesehen</b> markieren</button> -->
 					</div>
 				</div>
-			</div>
+			</div> -->
 			<pre v-if="settings.developerMode.value">
 				currentMovie: {{ currentMovie }}
 				currentSeason: {{ currentSeason }}
