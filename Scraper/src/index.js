724e0ef294a0fa71a041fdd20e4c9c8f3f1388bb
Jodu555 <Jodu505@gmail.com>
2023-01-15 10:08:21 +0100

    DUpdate

--- Scraper/src/index.js
+++ Scraper/src/index.js
@@ -28,14 +28,14 @@ socket.on('connect', async () => {
 	// Check if there are missing refenreces
 	// console.log(res.data.filter((d) => !Boolean(d.references.aniworld)).map((d) => ({ ID: d.ID, title: d.title })));
 
-	res.data = res.data.filter((x) => x.title.includes('Grace'));
+	// res.data = res.data.filter((x) => x.title.includes('Grace'));
 
 	// console.log(`res.data`, res.data);
 
 	compareForNewReleases(res.data);
 
 	// Sub manually print the infos out
-	// const anime = new Aniworld('https://aniworld.to/anime/stream/black-summoner');
+	// const anime = new Aniworld('https://aniworld.to/anime/stream/');
 	// const { url, informations } = await anime.parseInformations();
 
 	// const output = {
