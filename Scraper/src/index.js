39d628bd4fea6c8ac7ac8633aba2b813b8a6c888
Jodu555 <Jodu505@gmail.com>
2023-01-16 12:48:52 +0100

    Coded a manual recognition software to download

--- Scraper/src/index.js
+++ Scraper/src/index.js
@@ -20,6 +20,31 @@ socket.on('disconnect', () => {
 socket.on('connect', async () => {
 	console.log('Socket Connection: Connected');
 
+	// const seasons = [11, 10, 10, 10, 10, 10];
+
+	// const output = seasons
+	// 	.map((x, i) => {
+	// 		const s = i + 1;
+	// 		const out = [];
+	// 		for (let j = 0; j < x; j++) {
+	// 			const e = j + 1;
+	// 			out.push({
+	// 				_animeFolder: 'Rick and Morty',
+	// 				finished: false,
+	// 				folder: `Season ${s}`,
+	// 				file: `Rick and Morty St.${s} Flg.${e}_GerDub`,
+	// 				url: `http://190.115.18.20/serie/stream/rick-and-morty/staffel-${s}/episode-${e}`,
+	// 				m3u8: '',
+	// 			});
+	// 		}
+	// 		return out;
+	// 	})
+	// 	.flat();
+
+	// console.log(output.length);
+
+	// fs.writeFileSync('dlList.json', JSON.stringify(output, null, 3));
+
 	// const data = JSON.parse(fs.readFileSync('dlList.json', 'utf-8'));
 	// console.log(data.length);
 
@@ -35,48 +60,32 @@ socket.on('connect', async () => {
 
 	// compareForNewReleases(res.data);
 
-	const arr = [
-		{
-			title: 'The Maid I Recently Hired Is Mysterious',
-			url: 'the-maid-i-recently-hired-is-mysterious',
-		},
-		{
-			title: 'Domestic Girlfriend',
-			url: 'domestic-girlfriend',
-		},
-		{
-			title: 'The Eminence in Shadow',
-			url: 'the-eminence-in-shadow',
-		},
-		{
-			title: 'My Hero Academia',
-			url: 'my-hero-academia',
-		},
-		{
-			title: "Shikimori's Not Just a Cutie",
-			url: 'shikimoris-not-just-a-cutie',
-		},
-		{
-			title: 'Don’t Toy With Me, Miss Nagatoro',
-			url: 'dont-toy-with-me-miss-nagatoro',
-		},
-		{
-			title: 'Reincarnated as a Sword',
-			url: 'reincarnated-as-a-sword',
-		},
-	];
+	// const arr = [
+	// 	{
+	// 		title: "Shikimori's Not Just a Cutie",
+	// 		url: 'shikimoris-not-just-a-cutie',
+	// 	},
+	// 	{
+	// 		title: 'Don’t Toy With Me, Miss Nagatoro',
+	// 		url: 'dont-toy-with-me-miss-nagatoro',
+	// 	},
+	// 	{
+	// 		title: 'Reincarnated as a Sword',
+	// 		url: 'reincarnated-as-a-sword',
+	// 	},
+	// ];
 
-	const mappedArr = arr.map((x) => {
-		return {
-			ID: crypto.randomUUID().split('-')[0],
-			title: x.title,
-			references: { aniworld: 'https://aniworld.to/anime/stream/' + x.url },
-			seasons: [],
-			movies: [],
-		};
-	});
+	// const mappedArr = arr.map((x) => {
+	// 	return {
+	// 		ID: crypto.randomUUID().split('-')[0],
+	// 		title: x.title,
+	// 		references: { aniworld: 'https://aniworld.to/anime/stream/' + x.url },
+	// 		seasons: [],
+	// 		movies: [],
+	// 	};
+	// });
 
-	compareForNewReleases(mappedArr);
+	// compareForNewReleases(mappedArr);
 
 	// Sub manually print the infos out
 	// const anime = new Aniworld('https://aniworld.to/anime/stream/');
