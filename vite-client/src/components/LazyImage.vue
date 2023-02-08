ab9969ee1f348d691d1e7e22f5e20ceefc15fa91
Jodu555 <Jodu505@gmail.com>
2023-02-08 08:23:33 +0100

    added the lazy image loading

--- vite-client/src/components/LazyImage.vue
+++ vite-client/src/components/LazyImage.vue
@@ -1,13 +1,48 @@
 <template>
-	<img :src="props.src" :alt="props.alt" />
+	<div v-if="loading" class="text-center justtify-content-center mt-3">
+		<div class="spinner-border" style="width: 3rem; height: 3rem" role="status">
+			<span class="visually-hidden">Loading...</span>
+		</div>
+	</div>
+	<img v-else ref="image" :alt="props.alt" />
 </template>
 <script setup>
 import { onMounted, ref } from 'vue';
 
 const props = defineProps(['src', 'alt']);
 
-const recommendations = ref([]);
+const loading = ref(true);
 
-const id = ref(Math.ceil(Math.random() * 100000));
+const image = ref(null);
+
+function loadImage() {
+	image.value.src = props.src;
+}
+
+function createObserver() {
+	const options = {
+		root: null,
+		threshold: '0',
+	};
+	const observer = new IntersectionObserver((entries) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				loadImage();
+				observer.unobserve(image.value);
+			}
+		});
+	}, options);
+	observer.observe(image.value);
+}
+
+onMounted(() => {
+	if (window['IntersectionObserver']) {
+		console.log('Observer Created');
+		createObserver();
+	} else {
+		console.log('Loaded Image onMount');
+		loadImage();
+	}
+});
 </script>
 <style lang=""></style>
