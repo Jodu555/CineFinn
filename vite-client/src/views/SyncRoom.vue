0e74601dd637d41de57e1ea4305637a920e9552a
Jodu555 <Jodu505@gmail.com>
2023-02-11 13:48:57 +0100

    Worked on the sync system

--- vite-client/src/views/SyncRoom.vue
+++ vite-client/src/views/SyncRoom.vue
@@ -1,21 +1,37 @@
 <template>
-	<div class="container">
-		<button @click="leaveRoom()" type="button" class="btn btn-outline-danger">Leave Room</button>
-		<h1>Came</h1>
+	<div class="mt-3 container">
+		<div class="d-flex justify-content-between">
+			<button @click="leaveRoom()" type="button" class="btn btn-outline-danger">Leave Room</button>
+			<AutoComplete :options="{ placeholder: 'Select a Series...', clearAfterSelect: true }" :data="autoCompleteSeries" :select-fn="selectSeries" />
+		</div>
 	</div>
 </template>
 <script>
-import { mapActions } from 'vuex';
+import AutoComplete from '@/components/Layout/AutoComplete.vue';
+import { mapActions, mapState } from 'vuex';
 
 export default {
+	components: { AutoComplete },
+	computed: {
+		...mapState(['series']),
+		autoCompleteSeries() {
+			return this.series.map((x) => ({ value: x.title, ID: x.ID }));
+		},
+	},
 	methods: {
 		...mapActions('sync', ['leaveRoom']),
+		selectSeries(ID) {
+			this.$socket.emit('sync-selectSeries', { ID });
+		},
 	},
 	mounted() {
-		this.$socket.on('roomVideoAction', ({ action, value }) => {
-			if (action == 'playback') {
-			} else if (action == 'skip') {
-			} else if (action == 'skipPercent') {
+		this.$socket.on('sync-videoAction', ({ action, value }) => {
+			if (action == 'sync-playback') {
+				// value = true = Play
+				// Value = false = Pause
+			} else if (action == 'sync-skip') {
+			} else if (action == 'sync-skipPercent') {
+			} else if (action == 'sync-skipTimeline') {
 			}
 		});
 	},
