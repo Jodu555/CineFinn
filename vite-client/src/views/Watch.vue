7c2abffa06f021595ffc6bd8325eea69e457e856
Jodu555 <Jodu505@gmail.com>
2023-06-24 18:32:54 +0200

    extracted the markSeason dropdown into own component

--- vite-client/src/views/Watch.vue
+++ vite-client/src/views/Watch.vue
@@ -6,6 +6,7 @@
 		<div v-if="homeLoading || loading" class="d-flex justify-content-center">
 			<div class="spinner-border" role="status">
 				<span class="visually-hidden">Loading...</span>
+				import MarkSeasonDropdown from '../components/Watch/MarkSeasonDropdown.vue';
 			</div>
 		</div>
 		<div v-auto-animate class="container" v-if="currentSeries != undefined && currentSeries.ID != -1">
@@ -25,28 +26,7 @@
 			<div v-auto-animate v-if="showLatestWatchButton" class="text-center">
 				<button @click="skipToLatestTime" class="btn btn-outline-info">Jump to Latest watch position!</button>
 			</div>
-			<div v-if="currentSeason !== -1" class="d-flex justify-content-end">
-				<div class="btn-group">
-					<button
-						class="btn btn-outline-primary dropdown-toggle"
-						type="button"
-						id="seenmark"
-						data-bs-toggle="dropdown"
-						aria-haspopup="true"
-						aria-expanded="false"
-					>
-						Staffel schon Gesehen ?
-					</button>
-					<div class="dropdown-menu" aria-labelledby="seenmark">
-						<button type="button" class="dropdown-item" @click="markSeason(true)" :class="{ disabled: currentSeason == -1 }">
-							<b>Schon Gesehen</b>
-						</button>
-						<button type="button" class="dropdown-item" @click="markSeason(false)" :class="{ disabled: currentSeason == -1 }">
-							<b>Noch nicht gesehen</b>
-						</button>
-					</div>
-				</div>
-			</div>
+			<MarkSeasonDropdown />
 			<pre v-if="settings.developerMode.value">
 				currentMovie: {{ currentMovie }}
 				currentSeason: {{ currentSeason }}
@@ -99,9 +79,10 @@ import EntityListView from '@/components/Watch/EntityListView.vue';
 import ExtendedVideo from '@/components/Watch/ExtendedVideo.vue';
 import ControlInformationModal from '@/components/Watch/ControlInformationModal.vue';
 import EntityActionsInformation from '@/components/Watch/EntityActionsInformation.vue';
+import MarkSeasonDropdown from '@/components/Watch/MarkSeasonDropdown.vue';
 
 export default {
-	components: { EntityListView, ExtendedVideo, ControlInformationModal, EntityActionsInformation },
+	components: { EntityListView, ExtendedVideo, ControlInformationModal, EntityActionsInformation, MarkSeasonDropdown },
 	data() {
 		return {
 			cleanupFN: null,
