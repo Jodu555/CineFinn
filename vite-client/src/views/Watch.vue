467bb099f1e63fc8ec8dfe488f466e414f3edc5b
Jodu555 <Jodu505@gmail.com>
2023-06-24 17:41:38 +0200

    Removed the logging and fixed it up

--- vite-client/src/views/Watch.vue
+++ vite-client/src/views/Watch.vue
@@ -25,7 +25,7 @@
 			<div v-auto-animate v-if="showLatestWatchButton" class="text-center">
 				<button @click="skipToLatestTime" class="btn btn-outline-info">Jump to Latest watch position!</button>
 			</div>
-			<div class="d-flex justify-content-end">
+			<div v-if="currentSeason !== -1" class="d-flex justify-content-end">
 				<div class="btn-group">
 					<button
 						class="btn btn-outline-primary dropdown-toggle"
@@ -35,12 +35,14 @@
 						aria-haspopup="true"
 						aria-expanded="false"
 					>
-						Staffel Gesehen ?
+						Staffel schon Gesehen ?
 					</button>
 					<div class="dropdown-menu" aria-labelledby="seenmark">
-						<button type="button" class="dropdown-item" @click="markSeason(true)" :class="{ disabled: currentSeason == -1 }"><b>Gesehen</b></button>
+						<button type="button" class="dropdown-item" @click="markSeason(true)" :class="{ disabled: currentSeason == -1 }">
+							<b>Schon Gesehen</b>
+						</button>
 						<button type="button" class="dropdown-item" @click="markSeason(false)" :class="{ disabled: currentSeason == -1 }">
-							<b>Nicht gesehen</b>
+							<b>Noch nicht gesehen</b>
 						</button>
 					</div>
 				</div>
