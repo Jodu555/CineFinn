ce051bba8afb81b2f1bc4efa1f90e77a34170459
Jodu555 <Jodu505@gmail.com>
2023-09-06 02:33:48 +0200

    Started to integrate the new crawler

--- CineFinnServer-TS-Rewrite/src/utils/crawler.ts
+++ CineFinnServer-TS-Rewrite/src/utils/crawler.ts
@@ -5,15 +5,15 @@
 import path from 'path';
 import { v4 as uuidv4 } from 'uuid';
 import { listFiles } from './fileutils';
-import { Series } from '../classes/series';
-import { SerieObject } from '../types/classes';
+import { Episode, Movie, Series, filenameParser } from '../classes/series';
+import { SerieEpisodeObject, SerieObject } from '../types/classes';
 
 const generateID = () => {
 	return uuidv4().split('-')[0];
 	// return Math.floor(Math.random() * 10000);
 };
 
-const crawlAndIndex = () => {
+const oldCrawlAndIndex = () => {
 	const { Series, filenameParser, Episode, Movie } = require('../classes/series');
 
 	const overcategories = ['Aniworld', 'STO'];
@@ -90,6 +90,93 @@ const crawlAndIndex = () => {
 
 	//Strinify in Json and then parse to deal with the empty array items the they are null
 	series = JSON.parse(JSON.stringify(series));
+
+	series = series.map((e) => {
+		const newSeasons = e.seasons
+			.filter((x) => {
+				// x == null && console.log('innner', e, x, 'out', x == null ? [] : x);
+
+				// return x == null ? [] : x;
+				return x != null;
+			})
+			.map((x) => x.sort(sorterFunction));
+
+		return {
+			...e,
+			seasons: newSeasons,
+		};
+	});
+
+	return series;
+};
+
+const crawlAndIndex = () => {
+	const pathEntries = [process.env.VIDEO_PATH];
+
+	let files: string[] = [];
+
+	for (const pathEntrie of pathEntries) {
+		let { files: tmp_files } = listFiles(pathEntrie);
+		files.push(...tmp_files);
+		tmp_files = null;
+	}
+
+	// let { files: tmp2_files } = listFiles('Z:\\home\\laterIntegrate');
+	// files.push(...tmp2_files);
+	// tmp_files = null;
+
+	//Strip all non mp4 files from the files
+	files = files.filter((f) => path.parse(f).ext == '.mp4');
+
+	// console.log(categorieMap);
+
+	// Strip the dirs down and seperate between season or movie dirs or series dirs
+	let series: SerieObject[] = [];
+
+	files.forEach((e) => {
+		const base = path.parse(e).base;
+		const parsedData = filenameParser(e, base);
+
+		let item = series.find((x) => x.title.includes(parsedData.title));
+		if (item == undefined) {
+			const categorie = path.parse(path.join(path.parse(e).dir, '../../')).base;
+			const serie = new Series(generateID(), categorie, parsedData.title, [], []);
+			series.push(serie);
+			item = serie;
+		}
+		if (parsedData.movie == true) {
+			const existMovie = item.movies.find((x) => x.primaryName.includes(parsedData.movieTitle));
+			if (existMovie) {
+				existMovie.langs.push(parsedData.language);
+			} else {
+				const movie = new Movie(e, parsedData.movieTitle, '', [parsedData.language]);
+				item.movies.push(movie);
+			}
+		} else {
+			const currentArr = item.seasons[parsedData.season - 1];
+
+			const episode = new Episode(e, parsedData.title, '', parsedData.season, parsedData.episode, [parsedData.language]);
+
+			if (Array.isArray(currentArr)) {
+				const existEpisode = currentArr.find((eps) => eps.season == parsedData.season && eps.episode == parsedData.episode);
+				if (existEpisode) {
+					existEpisode.langs.push(parsedData.language);
+					return;
+				} else {
+					currentArr.push(episode);
+				}
+			} else {
+				item.seasons[parsedData.season - 1] = [episode];
+			}
+		}
+	});
+
+	const sorterFunction = (a: SerieEpisodeObject, b: SerieEpisodeObject) => {
+		return a.episode - b.episode;
+	};
+
+	//Strinify in Json and then parse to deal with the empty array items the they are null
+	// series = JSON.parse(JSON.stringify(series));
 
 	series = series.map((e) => {
 		const newSeasons = e.seasons
