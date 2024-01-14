df002fad4556a9ea13c3c9a85b0c11715ffcbb84
Jodu555 <Jodu505@gmail.com>
2024-01-14 20:38:14 +0100

    Experimented a bi with the ffprobe data

--- CineFinnServer-TS-Rewrite/src/ffprobeAnalyzer.ts
+++ CineFinnServer-TS-Rewrite/src/ffprobeAnalyzer.ts
@@ -1,4 +1,49 @@
 import fs from 'fs';
 import path from 'path';
 import dotenv from 'dotenv';
+import ffprobe from 'ffprobe';
+import { Series } from './classes/series';
 dotenv.config();
+
+const ratioMap = {};
+
+async function run() {
+    const series: Series[] = JSON.parse(fs.readFileSync('db.json', 'utf-8'));
+    // const serie = series.find(x => x.title.includes('Irregular'));
+    for (const serie of series) {
+        for (const season of serie.seasons) {
+            for (const ep of season) {
+                const stats = await ffprobe(ep.filePath, { path: 'C:\\ffmpeg\\bin\\ffprobe.exe' });
+                const vidStream = stats.streams.find(x => x.codec_type == 'video');
+
+                const { duration, avg_frame_rate, width, height } = vidStream
+
+                const key = `${width}x${height}`;
+                const videoStats = {
+                    path: ep.filePath,
+                    key,
+                    duration,
+                    fps: avg_frame_rate,
+                    width,
+                    height
+                }
+
+
+                if (ratioMap[videoStats.key]) {
+                    ratioMap[videoStats.key] += 1;
+                } else {
+                    ratioMap[videoStats.key] = 1;
+                }
+            }
+        }
+    }
+
+
+
+    console.log(ratioMap);
+
+
+}
+
+
+run();
