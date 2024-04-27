3429fadb1c98458cc80d49167b249cc3037387ce
Jodu555 <Jodu505@gmail.com>
2024-04-27 03:56:36 +0200

    Returned the Scraper to what i want in an upload

--- Scraper/src/index.ts
+++ Scraper/src/index.ts
@@ -152,20 +152,20 @@ async function addReference() {
 
 async function checkForUpdates() {
 	const res = await axios.get<Serie[]>('http://cinema-api.jodu555.de/index/all?auth-token=' + process.env.AUTH_TOKEN_REST);
-	res.data = res.data.filter((x) => x.ID == 'xxxxxxx');
+	// res.data = res.data.filter((x) => x.ID == 'xxxxxxx');
 	// res.data = res.data.filter((x) => x.title.includes('Honor') || x.title.includes('Grace'));
 	// res.data.length = res.data.length / 0.5;
 	// res.data.push(
 	// 	...([
 	// 		{
 	// 			ID: 'xxxxxxxxxxx',
-	// 			title: 'Hetalia - Axis Powers',
+	// 			title: 'Sugar Apple Fairy Tale',
 	// 			categorie: 'Aniworld',
 	// 			seasons: [],
 	// 			movies: [],
 	// 			infos: {},
 	// 			references: {
-	// 				zoro: '1379',
+	// 				zoro: '18477',
 	// 			},
 	// 		},
 	// 	] as Serie[])
