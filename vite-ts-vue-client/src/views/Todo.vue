3f9f15bf8ea97c6db5d5e4e490a60970422f18d7
Jodu555 <Jodu505@gmail.com>
2024-05-21 00:09:34 +0200

    added a image to the left of the todo item and it looks just gorgeus

--- vite-ts-vue-client/src/views/Todo.vue
+++ vite-ts-vue-client/src/views/Todo.vue
@@ -31,209 +31,223 @@
 			item-key="ID">
 			<template #item="{ element }: { element: TodoItem }">
 				<li class="list-group-item" v-auto-animate>
-					<div class="d-flex justify-content-between">
-						<div>
-							{{ element.name }} -
-							{{ element.categorie }}
-							<span class="badge bg-info mx-2 me-3">{{ element.order }}</span>
-							<button v-if="!element.edited" title="Edit" type="button" @click="element.edited = true" class="btn btn-outline-primary me-3">
-								<font-awesome-icon :icon="['fa-solid', 'fa-pen']" size="lg" />
-							</button>
-							<button v-if="!element.edited" title="Use" type="button" @click="useTodo(element.ID)" class="btn btn-outline-success me-3">
-								<font-awesome-icon :icon="['fa-solid', 'fa-check']" size="lg" />
-							</button>
-							<button v-if="!element.edited" title="Delete" type="button" @click="deleteTodo(element.ID)" class="btn btn-outline-danger">
-								<font-awesome-icon :icon="['fa-solid', 'fa-trash']" size="lg" />
-							</button>
-						</div>
-						<div>
-							<button
-								v-if="!element.edited && element.order > 6 && auth.userInfo.role > 2"
-								title="Bring to top"
-								@click="moveToDoToTop(element.ID)"
-								type="button"
-								class="btn btn-outline-info me-2">
-								<font-awesome-icon icon="fa-solid fa-up-long" size="l" />
-							</button>
-							<button
-								v-if="!element.edited && element.order <= state.list.length / 1.2"
-								title="Bring to Bottom"
-								@click="moveToDoToBottom(element.ID)"
-								type="button"
-								class="btn btn-outline-warning">
-								<font-awesome-icon icon="fa-solid fa-down-long" size="l" />
-							</button>
-							<button v-if="element.edited" type="button" @click="element.edited = false" class="btn btn-close"></button>
-						</div>
-					</div>
-					<!-- <h5>Infos</h5> -->
-					<span v-if="state.permittedAccounts.find((x) => x.UUID == element.creator) != null"
-						>- {{ state.permittedAccounts.find((x) => x.UUID == element.creator)?.username }}</span
-					>
-					<div style="width: 15%" class="d-flex justify-content-around">
-						<a v-if="element.references.aniworld" target="_blank" :href="element.references.aniworld" class="h6">A</a>
-						<span v-if="element.references.zoro" class="h6">Z</span>
-						<span v-if="element.references.sto" target="_blank" :href="element.references.aniworld" class="h6">S</span>
-					</div>
+					<div class="d-flex" v-auto-animate>
+						<img
+							v-if="
+								element.scraped != undefined &&
+								element.scraped !== true &&
+								element.scraped.informations.image &&
+								typeof element.scraped.informations.image == 'string'
+							"
+							:src="element.scraped.informations.image"
+							class="img-fluid rounded-top me-4 dp-img"
+							alt="" />
+						<div style="width: 100%" v-auto-animate>
+							<div class="d-flex justify-content-between">
+								<div>
+									{{ element.name }} -
+									{{ element.categorie }}
+									<span class="badge bg-info mx-2 me-3">{{ element.order }}</span>
+									<button v-if="!element.edited" title="Edit" type="button" @click="element.edited = true" class="btn btn-outline-primary me-3">
+										<font-awesome-icon :icon="['fa-solid', 'fa-pen']" size="lg" />
+									</button>
+									<button v-if="!element.edited" title="Use" type="button" @click="useTodo(element.ID)" class="btn btn-outline-success me-3">
+										<font-awesome-icon :icon="['fa-solid', 'fa-check']" size="lg" />
+									</button>
+									<button v-if="!element.edited" title="Delete" type="button" @click="deleteTodo(element.ID)" class="btn btn-outline-danger">
+										<font-awesome-icon :icon="['fa-solid', 'fa-trash']" size="lg" />
+									</button>
+								</div>
+								<div>
+									<button
+										v-if="!element.edited && element.order > 6 && auth.userInfo.role > 2"
+										title="Bring to top"
+										@click="moveToDoToTop(element.ID)"
+										type="button"
+										class="btn btn-outline-info me-2">
+										<font-awesome-icon icon="fa-solid fa-up-long" size="l" />
+									</button>
+									<button
+										v-if="!element.edited && element.order <= state.list.length / 1.2"
+										title="Bring to Bottom"
+										@click="moveToDoToBottom(element.ID)"
+										type="button"
+										class="btn btn-outline-warning">
+										<font-awesome-icon icon="fa-solid fa-down-long" size="l" />
+									</button>
+									<button v-if="element.edited" type="button" @click="element.edited = false" class="btn btn-close"></button>
+								</div>
+							</div>
+							<!-- <h5>Infos</h5> -->
+							<span v-if="state.permittedAccounts.find((x) => x.UUID == element.creator) != null"
+								>- {{ state.permittedAccounts.find((x) => x.UUID == element.creator)?.username }}</span
+							>
+							<div style="width: 15%" class="d-flex justify-content-around">
+								<a v-if="element.references.aniworld" target="_blank" :href="element.references.aniworld" class="h6">A</a>
+								<span v-if="element.references.zoro" class="h6">Z</span>
+								<span v-if="element.references.sto" target="_blank" :href="element.references.aniworld" class="h6">S</span>
+							</div>
 
-					<ul v-if="element.scraped != undefined && element.scraped !== true">
-						<li>Episodes: {{ element.scraped?.seasons?.flat()?.length }}</li>
-						<li>
-							&nbsp;&nbsp;&nbsp;&nbsp;Apx Size on Disk:
-							{{ numWithFP((element.scraped?.seasons?.flat()?.length * constants.mbperEpisode) / 1024, 1) }}GB
-						</li>
-						<li v-for="[key, value] in Object.entries(languageDevision(element))">&nbsp;&nbsp;&nbsp;&nbsp;{{ key }}: {{ value }}%</li>
-						<template v-if="element.scraped?.movies != undefined">
-							<li>Movies: {{ element.scraped?.movies?.length }}</li>
-							<li>
-								&nbsp;&nbsp;&nbsp;&nbsp;Apx Size on Disk:
-								{{ numWithFP((element.scraped?.movies?.length * constants.mbperMovie) / 1024, 1) }}GB
-							</li>
-						</template>
-						<li>
-							<em>
-								<template v-if="auth.userInfo.role > 2">
-									Source:
-									<br />
-									<a target="_blank" :href="element.scraped?.url">{{ element.scraped?.url }}</a>
-									<template v-if="element.scrapedZoro !== undefined && element.scrapedZoro !== true">
-										<br />
-										<a target="_blank" :href="element.scrapedZoro.episodes[0]?.url">{{ element.scrapedZoro.episodes[0]?.url }}</a>
-									</template>
+							<ul v-if="element.scraped != undefined && element.scraped !== true">
+								<li>Episodes: {{ element.scraped?.seasons?.flat()?.length }}</li>
+								<li>
+									&nbsp;&nbsp;&nbsp;&nbsp;Apx Size on Disk:
+									{{ numWithFP((element.scraped?.seasons?.flat()?.length * constants.mbperEpisode) / 1024, 1) }}GB
+								</li>
+								<li v-for="[key, value] in Object.entries(languageDevision(element))">&nbsp;&nbsp;&nbsp;&nbsp;{{ key }}: {{ value }}%</li>
+								<template v-if="element.scraped?.movies != undefined">
+									<li>Movies: {{ element.scraped?.movies?.length }}</li>
+									<li>
+										&nbsp;&nbsp;&nbsp;&nbsp;Apx Size on Disk:
+										{{ numWithFP((element.scraped?.movies?.length * constants.mbperMovie) / 1024, 1) }}GB
+									</li>
 								</template>
-								<br />
-								<p style="cursor: pointer" @click="deleteParsedInfos(element.ID)"><u>Delete Scraped infos</u></p>
-							</em>
-						</li>
-					</ul>
+								<li>
+									<em>
+										<template v-if="auth.userInfo.role > 2">
+											Source:
+											<br />
+											<a target="_blank" :href="element.scraped?.url">{{ element.scraped?.url }}</a>
+											<template v-if="element.scrapedZoro !== undefined && element.scrapedZoro !== true">
+												<br />
+												<a target="_blank" :href="element.scrapedZoro.episodes[0]?.url">{{ element.scrapedZoro.episodes[0]?.url }}</a>
+											</template>
+										</template>
+										<br />
+										<p style="cursor: pointer" @click="deleteParsedInfos(element.ID)"><u>Delete Scraped infos</u></p>
+									</em>
+								</li>
+							</ul>
 
-					<div v-if="element.scraped == true" class="m-3 d-flex justify-content-between">
-						<div class="spinner-border text-warning spinner-border-xs" role="status">
-							<span class="visually-hidden">Loading...</span>
-						</div>
-						<small class="text-danger" style="cursor: pointer" @click="retryScrapeTodo(element.ID)"><u>Retry</u></small>
-					</div>
+							<div v-if="element.scraped == true" class="m-3 d-flex justify-content-between">
+								<div class="spinner-border text-warning spinner-border-xs" role="status">
+									<span class="visually-hidden">Loading...</span>
+								</div>
+								<small class="text-danger" style="cursor: pointer" @click="retryScrapeTodo(element.ID)"><u>Retry</u></small>
+							</div>
 
-					<span v-if="element.scrapingError" class="h6 text-danger">
-						<span
-							>!!! {{ element.scrapingError }} !!! &nbsp;&nbsp;&nbsp;&nbsp;
-							<small style="cursor: pointer" @click="retryScrapeTodo(element.ID)">
-								<u>Retry</u>
-							</small>
-						</span>
-					</span>
+							<span v-if="element.scrapingError" class="h6 text-danger">
+								<span
+									>!!! {{ element.scrapingError }} !!! &nbsp;&nbsp;&nbsp;&nbsp;
+									<small style="cursor: pointer" @click="retryScrapeTodo(element.ID)">
+										<u>Retry</u>
+									</small>
+								</span>
+							</span>
 
-					<div v-if="element.edited">
-						<div class="row text-center mt-2 mb-2 align-items-center">
-							<div class="col-2">
-								<label for="name" class="form-label">Name:</label>
-							</div>
-							<div class="col-7">
-								<input
-									type="text"
-									:disabled="auth.userInfo.UUID != element.creator && auth.userInfo.role == 2"
-									class="form-control"
-									id="name"
-									v-model="element.name" />
-							</div>
-						</div>
-						<template v-if="auth.userInfo.role == 2">
-							<div class="row text-center mt-2 mb-2 align-items-center">
-								<div class="col-2">
-									<label for="name" class="form-label">Creator:</label>
+							<div v-if="element.edited">
+								<div class="row text-center mt-2 mb-2 align-items-center">
+									<div class="col-2">
+										<label for="name" class="form-label">Name:</label>
+									</div>
+									<div class="col-7">
+										<input
+											type="text"
+											:disabled="auth.userInfo.UUID != element.creator && auth.userInfo.role == 2"
+											class="form-control"
+											id="name"
+											v-model="element.name" />
+									</div>
 								</div>
-								<div class="col-1 h5">
-									<span>{{ state.permittedAccounts.find((x) => x.UUID == element.creator)?.username }}</span>
+								<template v-if="auth.userInfo.role == 2">
+									<div class="row text-center mt-2 mb-2 align-items-center">
+										<div class="col-2">
+											<label for="name" class="form-label">Creator:</label>
+										</div>
+										<div class="col-1 h5">
+											<span>{{ state.permittedAccounts.find((x) => x.UUID == element.creator)?.username }}</span>
+										</div>
+									</div>
+								</template>
+								<template v-else>
+									<div class="row text-center mt-2 mb-2 align-items-center">
+										<div class="col-2">
+											<label for="name" class="form-label">Creator:</label>
+										</div>
+										<div class="col-3">
+											<select v-model="element.creator" style="width: 100%" class="form-select" aria-label="Default select example">
+												<option selected disabled>From</option>
+												<option v-for="account in state.permittedAccounts" :value="account.UUID">{{ account.username }}</option>
+											</select>
+										</div>
+									</div>
+								</template>
+								<div class="row text-center mt-2 mb-2 align-items-center">
+									<div class="col-2">
+										<label for="name" class="form-label">Kategorie:</label>
+									</div>
+									<div class="col-3">
+										<select
+											v-model="element.categorie"
+											style="width: 100%"
+											:disabled="auth.userInfo.UUID != element.creator && auth.userInfo.role == 2"
+											class="form-select"
+											aria-label="Default select example">
+											<option selected disabled>Kategorie</option>
+											<option>Aniworld</option>
+											<option>STO</option>
+											<option>K-Drama</option>
+										</select>
+									</div>
 								</div>
-							</div>
-						</template>
-						<template v-else>
-							<div class="row text-center mt-2 mb-2 align-items-center">
-								<div class="col-2">
-									<label for="name" class="form-label">Creator:</label>
+								<hr />
+								<h5>References</h5>
+								<h6>Anime</h6>
+								<div class="row text-center align-items-center mb-4">
+									<div class="col-2">
+										<label for="url" class="form-label">Aniworld:</label>
+									</div>
+									<div class="col-7">
+										<input
+											type="text"
+											:disabled="auth.userInfo.UUID != element.creator && auth.userInfo.role == 2"
+											class="form-control"
+											id="url"
+											v-model="element.references.aniworld" />
+									</div>
 								</div>
-								<div class="col-3">
-									<select v-model="element.creator" style="width: 100%" class="form-select" aria-label="Default select example">
-										<option selected disabled>From</option>
-										<option v-for="account in state.permittedAccounts" :value="account.UUID">{{ account.username }}</option>
-									</select>
+								<div class="row text-center align-items-center">
+									<div class="col-2">
+										<label for="url" class="form-label">Zoro:</label>
+									</div>
+									<div class="col-7">
+										<input
+											type="text"
+											:disabled="auth.userInfo.UUID != element.creator && auth.userInfo.role == 2"
+											class="form-control"
+											id="url"
+											v-model="element.references.zoro" />
+									</div>
 								</div>
-							</div>
-						</template>
-						<div class="row text-center mt-2 mb-2 align-items-center">
-							<div class="col-2">
-								<label for="name" class="form-label">Kategorie:</label>
-							</div>
-							<div class="col-3">
-								<select
-									v-model="element.categorie"
-									style="width: 100%"
-									:disabled="auth.userInfo.UUID != element.creator && auth.userInfo.role == 2"
-									class="form-select"
-									aria-label="Default select example">
-									<option selected disabled>Kategorie</option>
-									<option>Aniworld</option>
-									<option>STO</option>
-									<option>K-Drama</option>
-								</select>
-							</div>
-						</div>
-						<hr />
-						<h5>References</h5>
-						<h6>Anime</h6>
-						<div class="row text-center align-items-center mb-4">
-							<div class="col-2">
-								<label for="url" class="form-label">Aniworld:</label>
-							</div>
-							<div class="col-7">
-								<input
-									type="text"
-									:disabled="auth.userInfo.UUID != element.creator && auth.userInfo.role == 2"
-									class="form-control"
-									id="url"
-									v-model="element.references.aniworld" />
-							</div>
-						</div>
-						<div class="row text-center align-items-center">
-							<div class="col-2">
-								<label for="url" class="form-label">Zoro:</label>
-							</div>
-							<div class="col-7">
-								<input
-									type="text"
-									:disabled="auth.userInfo.UUID != element.creator && auth.userInfo.role == 2"
-									class="form-control"
-									id="url"
-									v-model="element.references.zoro" />
-							</div>
-						</div>
-						<hr />
-						<h6>Other</h6>
-						<div class="row text-center align-items-center mb-4">
-							<div class="col-2">
-								<label for="url" class="form-label">STO:</label>
-							</div>
-							<div class="col-7">
-								<input
-									type="text"
-									:disabled="auth.userInfo.UUID != element.creator && auth.userInfo.role == 2"
-									class="form-control"
-									id="url"
-									v-model="element.references.sto" />
-							</div>
-						</div>
+								<hr />
+								<h6>Other</h6>
+								<div class="row text-center align-items-center mb-4">
+									<div class="col-2">
+										<label for="url" class="form-label">STO:</label>
+									</div>
+									<div class="col-7">
+										<input
+											type="text"
+											:disabled="auth.userInfo.UUID != element.creator && auth.userInfo.role == 2"
+											class="form-control"
+											id="url"
+											v-model="element.references.sto" />
+									</div>
+								</div>
 
-						<div class="d-flex justify-content-end">
-							<button type="button" @click="element.edited = false" class="btn btn-outline-danger mx-2">Cancel</button>
-							<button
-								type="button"
-								@click="
-									element.edited = false;
-									save();
-								"
-								class="btn btn-outline-success">
-								Save
-							</button>
+								<div class="d-flex justify-content-end">
+									<button type="button" @click="element.edited = false" class="btn btn-outline-danger mx-2">Cancel</button>
+									<button
+										type="button"
+										@click="
+											element.edited = false;
+											save();
+										"
+										class="btn btn-outline-success">
+										Save
+									</button>
+								</div>
+							</div>
 						</div>
 					</div>
 				</li>
@@ -545,7 +559,7 @@ const pushTodoListUpdate = async () => {
 	useSocket().emit('todoListUpdate', saveList);
 };
 </script>
-<style>
+<style scoped lang="scss">
 .flip-list-move {
 	transition: transform 0.5s;
 }
@@ -561,5 +575,15 @@ const pushTodoListUpdate = async () => {
 
 .list-group-item {
 	cursor: move;
+}
+
+.dp-img {
+	max-width: 18rem;
+	min-width: 17rem;
+	min-height: 24rem;
+	max-height: 20rem;
+	width: 100%;
+	object-fit: cover;
+	border-radius: 25px;
 }
 </style>
