0e26a4d8d5a2f3d1ffab921295c3525c1484acfc
Jodu555 <Jodu505@gmail.com>
2024-06-01 23:19:59 +0200

    Update

--- Scraper/src/index.ts
+++ Scraper/src/index.ts
@@ -93,7 +93,7 @@ socket.on('connect', async () => {
 
 	// console.log(infos.seasons[0][16]);
 
-	await checkForUpdates();
+	// await checkForUpdates();
 	// await manuallyCraftTheList();
 	// await generateNewDownloadList();
 	// await manuallyPrintTheInfosOut();
