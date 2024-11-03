4d434e02076b94fc58da77bbe68de6a7fe0a886b
Jodu555 <Jodu505@gmail.com>
2024-11-03 12:23:15 +0100

    Updated the LazyImage Component to wait some time and check if element is still in view to account for the users quickly scrolling the element out of view again

--- vite-ts-vue-client/src/components/LazyImage.vue
+++ vite-ts-vue-client/src/components/LazyImage.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
 	<div>
 		<div v-if="loading" class="text-center justtify-content-center mt-3">
 			<div class="spinner-border" style="width: 3rem; height: 3rem" role="status">
@@ -40,6 +40,70 @@ function createObserver() {
 			}
 		});
 	}, options);
+	observer.observe(image.value);
+}
+
+onMounted(() => {
+	if (window['IntersectionObserver']) {
+		createObserver();
+	} else {
+		loadImage();
+	}
+});
+</script> -->
+<template>
+	<div>
+		<div v-if="loading" class="text-center justify-content-center mt-3">
+			<div class="spinner-border" style="width: 3rem; height: 3rem" role="status">
+				<span class="visually-hidden">Loading...</span>
+			</div>
+		</div>
+		<img ref="image" :class="props.childclass" :alt="props.alt" />
+	</div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+const props = defineProps(['src', 'alt', 'childclass']);
+const loading = ref(true);
+const image = ref<HTMLImageElement | null>(null);
+let loadTimeout: ReturnType<typeof setTimeout> | null = null;
+
+function loadImage() {
+	if (!image.value || loading.value == false) return;
+	image.value.addEventListener('load', () => {
+		loading.value = false;
+	});
+	image.value.addEventListener('error', () => console.log('Error on Loading Image', props.src));
+	image.value.src = props.src;
+}
+
+function createObserver() {
+	if (!image.value) return;
+
+	const options = {
+		root: null,
+		threshold: 0,
+	};
+
+	const observer = new IntersectionObserver((entries) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				loadTimeout = setTimeout(() => {
+					if (entry.isIntersecting) {
+						loadImage();
+					}
+				}, 50);
+			} else {
+				if (loadTimeout) {
+					clearTimeout(loadTimeout);
+					loadTimeout = null;
+				}
+			}
+		});
+	}, options);
+
 	observer.observe(image.value);
 }
 
