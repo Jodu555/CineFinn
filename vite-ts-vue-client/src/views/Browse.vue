c41d9053bd5aefb95af917c6e6c22405c77d04cc
Jodu555 <Jodu505@gmail.com>
2024-11-03 12:31:05 +0100

    if there is nothing found for continuewatching or watch again then artifically client side add a random list

--- vite-ts-vue-client/src/views/Browse.vue
+++ vite-ts-vue-client/src/views/Browse.vue
@@ -23,6 +23,7 @@ import { useAxios, useBaseURL } from '@/utils';
 import type { BrowseSerie, Serie } from '@/types';
 
 import VideoCarousel from '@/components/Home/VideoCarousel.vue';
+import { title } from 'process';
 
 const auth = useAuthStore();
 const index = useIndexStore();
@@ -30,23 +31,36 @@ const index = useIndexStore();
 const list: Record<string, { title: string; data: BrowseSerie[] }> = reactive({});
 
 onMounted(async () => {
-	const response = await useAxios().get('/recommendation');
+	const response = await useAxios().get<Record<'newest' | 'continueWatching' | 'watchagain' | string, { title: string; data: string[] }>>(
+		'/recommendation'
+	);
 	if (response.status != 200) return;
 	const json = response.data;
-	Object.keys(json).forEach((k) => {
-		list[k] = {
-			title: json[k].title,
-			data: json[k].data.map((ID: string) => {
+	if (json.continueWatching.data.length < 2 || json.watchagain.data.length < 2) {
+		json.random = {
+			title: 'Random',
+			data: index.series
+				.filter((x) => x.infos?.image == true)
+				.map((x) => x.ID)
+				.sort(() => Math.random() - 0.5)
+				.slice(0, 9),
+		};
+	}
+	console.log(json);
+
+	Object.keys(json).forEach((key) => {
+		list[key] = {
+			title: json[key].title,
+			data: json[key].data.map((ID: string) => {
 				const entity = index.series.find((z) => z.ID == ID) as BrowseSerie | undefined;
 				if (entity == undefined) {
-					console.log('Not found entity', k, ID, entity);
+					console.log('Not found entity', key, ID, entity);
 					return undefined;
 				}
 				if (entity?.infos?.image == true) {
 					const url = new URL(useBaseURL() + `/images/${entity.ID}/cover.jpg`);
 					url.searchParams.append('auth-token', auth.authToken);
 					entity.url = url.href;
-					// f.url = `http://cinema-api.jodu555.de/images/${f.ID}/cover.jpg?auth-token=${'SECR-DEV'}`;
 				} else {
 					entity.url = entity?.infos?.imageURL;
 				}
