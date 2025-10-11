63fe6f67948ca7790a5c1ba099dae0adc2784e1b
Jodu555 <Jodu505@gmail.com>
2025-10-11 06:32:04 +0200

    Added a vite client for now but now i will try nuxtr for the last time (way less copy paste to just make sure i give it a fair shot)

--- CineFinnClient/app/plugins/bootstrap.client.ts
+++
@@ -1,8 +0,0 @@
-// @ts-ignore
-// import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.min';
-// import 'bootstrap';
-import Swal from 'sweetalert2';
-export default defineNuxtPlugin(nuxtApp => {
-    // nuxtApp.provide('swal', Swal);
-    // nuxtApp.provide('bs', bootstrap);
-});
