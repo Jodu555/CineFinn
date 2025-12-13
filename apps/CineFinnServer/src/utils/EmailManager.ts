667903da228d411cec2453f21c8243ec78d9493f
Jodu555 <Jodu505@gmail.com>
2025-12-13 18:22:46 +0100

    changed the branding in verify email and removed the 48 hours token stuff

--- apps/CineFinnServer/src/utils/EmailManager.ts
+++ apps/CineFinnServer/src/utils/EmailManager.ts
@@ -21,137 +21,137 @@ import { generateEmailID } from './IdGenerators.js';
 // type DataType<T> = T extends 'VERIFICATION' ? { email: string; verificationToken: string; } : never;
 
 type DataType<T> =
-    T extends 'VERIFICATION' ? { verificationToken: string; } :
-    undefined;
+  T extends 'VERIFICATION' ? { verificationToken: string; } :
+  undefined;
 
 export default class EmailManager {
-    transporter: nodemailer.Transporter;
-    ready: boolean = false;
-    inflight: boolean = false;
-    constructor() {
-        const globalConfig = getConfig();
-        const config = {
-            service: 'gmail',
-            host: globalConfig.smtp.host,
-            port: globalConfig.smtp.port,
-            secure: globalConfig.smtp.secure,
-            auth: {
-                user: globalConfig.smtp.auth.user,
-                pass: globalConfig.smtp.auth.pass,
-            },
-        };
+  transporter: nodemailer.Transporter;
+  ready: boolean = false;
+  inflight: boolean = false;
+  constructor() {
+    const globalConfig = getConfig();
+    const config = {
+      service: 'gmail',
+      host: globalConfig.smtp.host,
+      port: globalConfig.smtp.port,
+      secure: globalConfig.smtp.secure,
+      auth: {
+        user: globalConfig.smtp.auth.user,
+        pass: globalConfig.smtp.auth.pass,
+      },
+    };
 
-        this.transporter = nodemailer.createTransport(config);
+    this.transporter = nodemailer.createTransport(config);
 
-        let outherThis = this;
-        this.transporter.verify(function (error, success) {
-            console.log({ error, success });
+    let outherThis = this;
+    this.transporter.verify(function (error, success) {
+      console.log({ error, success });
 
-            if (error) {
-                console.log('Error verifying email transporter:', error);
-            }
-            if (success) {
-                console.log('Email transporter is ready');
-                outherThis.ready = true;
-                outherThis.processEmails();
-            }
-        });
+      if (error) {
+        console.log('Error verifying email transporter:', error);
+      }
+      if (success) {
+        console.log('Email transporter is ready');
+        outherThis.ready = true;
+        outherThis.processEmails();
+      }
+    });
+  }
+
+  async processEmails() {
+    // console.log('Processing emails...', this.ready, this.inflight);
+    if (!this.ready) {
+      console.log('Email transporter not ready, skipping email processing');
+      setTimeout(() => this.processEmails(), 1000 * 60 * 5); // Retry in 5 minutes
+      return;
+    }
+    if (this.inflight) {
+      console.log('Email processing already in flight, skipping this round');
+      setTimeout(() => this.processEmails(), 1000 * 60 * 5); // Retry in 5 minutes
+      return;
+    }
+    const emails = await emailsTable.get({ status: 'PENDING' });
+    for (const email of emails) {
+      if (email.status == 'PENDING') {
+        await this.deepSendEmail(email);
+      }
+    }
+    setTimeout(() => this.processEmails(), 1000 * 60 * 5); // Check for stall emails every 5 minutes
+  }
+
+  async sendEmail<T extends EmailTypes>(account_UUID: string, email_type: T, data: DataType<T> & { email?: string; }) {
+    this.inflight = true;
+    try {
+
+      data.email = data.email || (await accountsTable.getOne({ UUID: account_UUID }))?.email;
+      const obj = await this.getEmailData(email_type, data);
+
+      const email: Email = {
+        UUID: generateEmailID(),
+        account_UUID,
+        email_type,
+        status: 'PENDING',
+        data: JSON.stringify(data),
+        sent_at: -1,
+        created_at: Date.now(),
+        ...obj,
+      } satisfies Email;
+
+      await emailsTable.create(email);
+
+      await this.deepSendEmail(email);
+    } catch (error) {
+      console.log('Error sending email:', error, account_UUID, email_type, data);
+    } finally {
+      this.inflight = false;
+    }
+  }
+
+  async getEmailData<T extends EmailTypes>(email_type: T, data: any): Promise<{ subject: string; html: string; text: string; }> {
+    if (email_type === 'VERIFICATION') {
+      return this.generateEmailVerification(data);
+    }
+    throw new Error('Email type not found');
+  }
+
+  private async deepSendEmail(email: Email) {
+
+    if (typeof email.data === 'string') {
+      email.data = JSON.parse(email.data);
     }
 
-    async processEmails() {
-        // console.log('Processing emails...', this.ready, this.inflight);
-        if (!this.ready) {
-            console.log('Email transporter not ready, skipping email processing');
-            setTimeout(() => this.processEmails(), 1000 * 60 * 5); // Retry in 5 minutes
-            return;
-        }
-        if (this.inflight) {
-            console.log('Email processing already in flight, skipping this round');
-            setTimeout(() => this.processEmails(), 1000 * 60 * 5); // Retry in 5 minutes
-            return;
-        }
-        const emails = await emailsTable.get({ status: 'PENDING' });
-        for (const email of emails) {
-            if (email.status == 'PENDING') {
-                await this.deepSendEmail(email);
-            }
-        }
-        setTimeout(() => this.processEmails(), 1000 * 60 * 5); // Check for stall emails every 5 minutes
+    if (!this.ready) {
+      console.log('Email transporter not ready for', email.UUID);
+      return;
     }
 
-    async sendEmail<T extends EmailTypes>(account_UUID: string, email_type: T, data: DataType<T> & { email?: string; }) {
-        this.inflight = true;
-        try {
-
-            data.email = data.email || (await accountsTable.getOne({ UUID: account_UUID }))?.email;
-            const obj = await this.getEmailData(email_type, data);
-
-            const email: Email = {
-                UUID: generateEmailID(),
-                account_UUID,
-                email_type,
-                status: 'PENDING',
-                data: JSON.stringify(data),
-                sent_at: -1,
-                created_at: Date.now(),
-                ...obj,
-            } satisfies Email;
-
-            await emailsTable.create(email);
-
-            await this.deepSendEmail(email);
-        } catch (error) {
-            console.log('Error sending email:', error, account_UUID, email_type, data);
-        } finally {
-            this.inflight = false;
+    this.transporter.sendMail(
+      {
+        from: process.env.MAIL_APP_MAIL,
+        to: (email.data as any).email,
+        subject: email.subject,
+        html: email.html,
+        text: email.text,
+      },
+      function (error, info) {
+        if (error) {
+          console.log(error);
+          return;
+        } else {
+          console.log('Email sent:', { ID: email.UUID, subject: email.subject, to: (email.data as any).email });
+          email.sent_at = Date.now();
+          email.status = 'SENT';
+          email.data = JSON.stringify(email.data);
+          emailsTable.update({ UUID: email.UUID }, email);
         }
-    }
+      }
+    );
+  }
 
-    async getEmailData<T extends EmailTypes>(email_type: T, data: any): Promise<{ subject: string; html: string; text: string; }> {
-        if (email_type === 'VERIFICATION') {
-            return this.generateEmailVerification(data);
-        }
-        throw new Error('Email type not found');
-    }
+  generateEmailVerification(data: DataType<'VERIFICATION'> & { email: string; }) {
+    const { email, verificationToken } = data;
 
-    private async deepSendEmail(email: Email) {
-
-        if (typeof email.data === 'string') {
-            email.data = JSON.parse(email.data);
-        }
-
-        if (!this.ready) {
-            console.log('Email transporter not ready for', email.UUID);
-            return;
-        }
-
-        this.transporter.sendMail(
-            {
-                from: process.env.MAIL_APP_MAIL,
-                to: (email.data as any).email,
-                subject: email.subject,
-                html: email.html,
-                text: email.text,
-            },
-            function (error, info) {
-                if (error) {
-                    console.log(error);
-                    return;
-                } else {
-                    console.log('Email sent:', { ID: email.UUID, subject: email.subject, to: (email.data as any).email });
-                    email.sent_at = Date.now();
-                    email.status = 'SENT';
-                    email.data = JSON.stringify(email.data);
-                    emailsTable.update({ UUID: email.UUID }, email);
-                }
-            }
-        );
-    }
-
-    generateEmailVerification(data: DataType<'VERIFICATION'> & { email: string; }) {
-        const { email, verificationToken } = data;
-
-        const html = `<!DOCTYPE html>
+    const html = `<!DOCTYPE html>
           <html lang="en">
           <head>
             <meta charset="UTF-8">
@@ -228,36 +228,32 @@ export default class EmailManager {
                   <h3 class="code">${verificationToken}</h3>
                 </div>
                 
-                <p>This verification code will expire in 48 hours. You then have to reregister!</p>
-                
                 <p>If you didn't create an account, you can safely ignore this email.</p>
                 
-                <p>Best regards,<br>The TwitchRecorder App</p>
+                <p>Best regards,<br>The CineFinn App</p>
               </div>
               <div class="email-footer">
-                <p>© ${new Date().getFullYear()} TwitchStreamRecorder. All rights reserved.</p>
+                <p>© ${new Date().getFullYear()} CineFinn. All rights reserved.</p>
                 <p>If you need any assistance, please just reply to this email!</p>
               </div>
             </div>
           </body>
           </html>`;
 
-        const text = `
+    const text = `
           Hello ${email},
           
           Thank you for signing up! To complete your registration, please input the following code on the registration page:
           
           ${verificationToken}
           
-          This verification token will expire in 48 hours. You then have to reregister!
-          
           If you didn't create an account, you can safely ignore this email.
           
           Best regards,
-          The TwitchStreamRecorder App
+          The CineFinn App
         `;
 
-        return { subject: 'Email Verification', html, text };
-    }
+    return { subject: 'Email Verification', html, text };
+  }
 
 }
