ff7adfc871650e9e1160ca29b0fbcabbdcd8f88c
Jodu555 <Jodu505@gmail.com>
2026-02-09 22:34:40 +0100

    Updated nuxt error page to look a bit better and have some additional infos on 404

--- apps/CineFinnClientNuxt/app/error.vue
+++ apps/CineFinnClientNuxt/app/error.vue
@@ -5,11 +5,12 @@ const props = defineProps<{ error: NuxtError }>();
 </script>
 
 <template>
-	<div class="container text-center">
-		<div>
-			<h1>{{ error.statusCode }}</h1>
-			<p>{{ error.message }}</p>
-			<NuxtLink to="/login">To login</NuxtLink>
+	<div class="d-flex align-items-center justify-content-center min-vh-100 px-2">
+		<div class="text-center">
+			<h1 class="display-1 fw-bold">{{ error.statusCode }}</h1>
+			<p class="fs-2 fw-medium mt-4">Oops! {{ error.message }}</p>
+			<p v-if="error.statusCode === 404" class="mt-4 mb-5">The page you're looking for doesn't exist or has been moved.</p>
+			<NuxtLink to="/" class="btn btn-light fw-semibold rounded-pill px-4 py-2 custom-btn"> Go Home </NuxtLink>
 		</div>
 	</div>
 </template>
