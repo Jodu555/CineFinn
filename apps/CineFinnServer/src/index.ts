a64d0ed18086047fa97b87a114c6617c86a8e9e2
Jodu555 <Jodu505@gmail.com>
2026-05-27 19:31:42 +0200

    Fix

--- apps/CineFinnServer/src/index.ts
+++ apps/CineFinnServer/src/index.ts
@@ -55,8 +55,7 @@ export const app = new Hono({
     }))
     .route('/health', healthRouter)
     .get('/status', async (c) => {
-        c.status(200);
-        c.body('OK');
+        return c.status(200);
     })
     .route('/auth', authRouter)
     .route('/index', indexRouter)
