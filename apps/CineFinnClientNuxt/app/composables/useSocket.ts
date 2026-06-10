474b4ac8fe69a73b580563495f26264195a628cf
Jodu555 <Jodu505@gmail.com>
2026-06-10 17:26:28 +0200

    Huge socket.io

--- apps/CineFinnClientNuxt/app/composables/useSocket.ts
+++ apps/CineFinnClientNuxt/app/composables/useSocket.ts
@@ -23,7 +23,7 @@ export default function useSocket(type: 'client' | 'rmvcEmitter' = 'client') {
     });
 
     socket = io(useAPIURL(), {
-        transports: ['websocket'],
+        // transports: ['websocket'],
         upgrade: true,
         reconnection: true,
         autoConnect: false,
